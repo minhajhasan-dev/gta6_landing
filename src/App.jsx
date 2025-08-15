653c72090f9be47b932a6a1d6c018f5c7671f599
minhajhasan-dev <minhajhasan.dev@gmail.com>
2025-08-16 05:36:45 +0600

    implement postcard

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
 import Lucia from "./sections/Lucia";
+import PostCard from "./sections/PostCard";
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Jason/>
         <SecondVideo/>
         <Lucia/>
+        <PostCard/>
       </div>
     </main>
   );
